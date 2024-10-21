86413d40196de501b004ad37f9ead443906cabcf
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-10-21 20:18:38 +0530

    add copy to clipboard functionality and toast

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import {ToastContainer , toast } from 'react-toastify'
 
 function App() {
   const [url, setUrl] = useState("");
   const [shortenUrl, setShortenUrl] = useState("");
+
+  const urlRef = useRef(null)
 
   const handleShorturl = async () => {
     const response = await fetch("http://localhost:8000/api/url", {
@@ -16,8 +19,17 @@ function App() {
     setShortenUrl(data);
   };
 
+
+  const copyLinkToClipboard = ()=>{
+    window.navigator.clipboard.writeText(shortenUrl);
+    toast.success("Link Succesfully copied to clipboard ")
+    // setShortenUrl("")
+  }
+
   return (
+    
     <div className='bg-black h-screen w-screen flex flex-col justify-center items-center p-4'>
+
       <h1 className='text-blue-700 text-3xl sm:text-4xl font-bold mb-6 text-center'>URL Shortener</h1>
       <div className='flex flex-col w-full max-w-md'>
         <input
@@ -40,7 +52,14 @@ function App() {
           className='text-2xl h-12 bg-white w-full text-center rounded-lg' 
           disabled 
           value={shortenUrl || ""}
+          ref={urlRef}
         />
+        <button 
+        className='bg-orange-500 ml-4 p-2 rounded-lg font-bold  hover:bg-green-500'
+        onClick={copyLinkToClipboard}
+        >
+          Copy 
+        </button>
       </div>
     </div>
   );
