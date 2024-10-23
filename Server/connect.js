f88e164eda59854279c7961cc950c5445015d0bd
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-10-23 20:20:15 +0530

    add env and mongodb altlas config

--- Server/connect.js
+++ Server/connect.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 
 const connectToDb = async ()=>{
-    return mongoose.connect( process.env.MONGO_URL)
+    const url = process.env.MONGO_URL
+    if(!url){
+        throw new Error('Please provide a mongo url')
+    }
+    try {
+    return await mongoose.connect( url).then(()=>{
+        console.log("Connected to MongoDB")
+    })
+        
+    } catch (error) {
+        console.log(`Mongodb connection Error : ${error.message}`)
+    }
 }
 
 export default connectToDb
