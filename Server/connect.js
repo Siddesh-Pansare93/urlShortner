e9866463b5ccb4c931ab04fb26dee63861b8dc3d
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-10-23 20:54:45 +0530

    --

--- Server/connect.js
+++ Server/connect.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDb = async ()=>{
     const url = process.env.MONGO_URL
+    console.log(url)
     if(!url){
         throw new Error('Please provide a mongo url')
     }
